a77d5f729982ff0df3eed0bd4971dabe39969183
SaugatMgr <saugatmagar433@gmail.com>
2023-05-18 10:54:56 +0545

    HomePage view

--- News/views.py
+++ News/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+class NewsHomePageView(ListView):
+    model = Post
+    template_name = "AZnews/home.html"
+    context_object_name = "posts"
