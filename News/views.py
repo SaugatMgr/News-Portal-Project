1c9509a33bf6325ed5ff47cfa7f1971afb1463b4
SaugatMgr <saugatmagar433@gmail.com>
2023-05-18 15:49:42 +0545

    context for trending portion of News

--- News/views.py
+++ News/views.py
@@ -2,7 +2,26 @@ from django.views.generic import ListView
 
 from .models import Post
 
+
 class NewsHomePageView(ListView):
     model = Post
     template_name = "AZnews/home.html"
     context_object_name = "posts"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['trending_post'] = (
+            Post.objects.filter(status='active', published_date__isnull=False)
+            .order_by('-views_count')
+            .first()
+        )
+        context['trending_posts'] = (
+            Post.objects.filter(status='active', published_date__isnull=False)
+            .order_by('-views_count')[1:4]
+        )
+        context['trending_now'] = (
+            Post.objects.filter(status='active', published_date__isnull=False)
+            .order_by('published_date', '-views_count')[:4]
+        )
+        return context
