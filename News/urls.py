da6445063ad6ef6705d0aeb32e6a710ca4236b71
SaugatMgr <saugatmagar433@gmail.com>
2023-05-19 21:51:35 +0545

    required urls for views of News app

--- News/urls.py
+++ News/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import NewsHomePageView
+from .views import (
+    NewsHomePageView,
+    PostListView,
+    PostByCategoryView,
+    PostByTagView,
+)
 
 urlpatterns = [
     path('', NewsHomePageView.as_view(), name="home"),
+    path('post-list/', PostListView.as_view(), name="post-list"),
+    path('post-by-category/<int:category_id>/',
+         PostByCategoryView.as_view(), name="post-by-category"),
+    path('post-by-tag/<int:tag_id>/',
+         PostByTagView.as_view(), name="post-by-tag"),
 ]
