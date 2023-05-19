5ce6e96d57ff5e1d8b8c664a41480f5eb168d137
SaugatMgr <saugatmagar433@gmail.com>
2023-05-19 21:51:13 +0545

    Necessary views for post features

--- News/views.py
+++ News/views.py
@@ -6,7 +6,22 @@ from django.utils import timezone
 from .models import Post, Category, Tag
 
 
-class NewsHomePageView(ListView):
+class CustomMixins:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['trending_now'] = (
+            Post.objects.filter(status='active', published_date__isnull=False)
+            .order_by('published_date', '-views_count')[:4]
+        )
+
+        context['categories'] = Category.objects.all()[:5]
+        context['tags'] = Tag.objects.all()[:10]
+
+        return context
+
+
+class NewsHomePageView(CustomMixins, ListView):
     model = Post
     template_name = "AZnews/home.html"
     context_object_name = "posts"
@@ -23,10 +38,6 @@ class NewsHomePageView(ListView):
             Post.objects.filter(status='active', published_date__isnull=False)
             .order_by('-views_count')[1:4]
         )
-        context['trending_now'] = (
-            Post.objects.filter(status='active', published_date__isnull=False)
-            .order_by('published_date', '-views_count')[:4]
-        )
 
         week_ago = timezone.now() - timedelta(days=7)
 
@@ -35,8 +46,48 @@ class NewsHomePageView(ListView):
                 status='active', published_date__isnull=False, published_date__gte=week_ago)
             .order_by('-published_date', '-views_count')[:7]
         )
-        
+
         context['categories'] = Category.objects.all()[:5]
         context['tags'] = Tag.objects.all()[:10]
-        
+
         return context
+
+
+class PostListView(CustomMixins, ListView):
+    model = Post
+    template_name = 'AZnews/main/list/list.html'
+    queryset = Post.objects.filter(
+        status='active',
+        published_date__isnull=False,
+    )
+    context_object_name = 'posts'
+
+
+class PostByCategoryView(CustomMixins, ListView):
+    model = Post
+    template_name = 'AZnews/main/list/list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = queryset.filter(
+            status='active',
+            published_date__isnull=False,
+            category=self.kwargs["category_id"],
+        )
+        return query
+
+
+class PostByTagView(CustomMixins, ListView):
+    model = Post
+    template_name = 'AZnews/main/list/list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = queryset.filter(
+            status='active',
+            published_date__isnull=False,
+            tag=self.kwargs["tag_id"],
+        )
+        return query
