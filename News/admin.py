6a0d005712a1fb460b1f4b9d7d2224b257673699
SaugatMgr <saugatmagar433@gmail.com>
2023-05-23 21:46:42 +0545

    registered NewsLetter model to admin panel

--- News/admin.py
+++ News/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Post, Category, Tag, Contact
+from .models import Post, Category, Tag, Contact, NewsLetter
 
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Contact)
+admin.site.register(NewsLetter)
