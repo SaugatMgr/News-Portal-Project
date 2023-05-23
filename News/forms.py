e1f298f42fe8acf903f6387d4d3775784dcd74ce
SaugatMgr <saugatmagar433@gmail.com>
2023-05-23 21:47:10 +0545

    added NewsLetter form

--- News/forms.py
+++ News/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from .models import Contact
+from .models import Contact, NewsLetter
 
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
+        fields = "__all__"
+
+
+class NewsLetterForm(forms.ModelForm):
+    class Meta:
+        model = NewsLetter
         fields = "__all__"
