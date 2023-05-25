1d138560ab47fbc27c2b2b1d81e669cf40ed9c06
SaugatMgr <saugatmagar433@gmail.com>
2023-05-25 19:49:57 +0545

    registered comment model

--- News/admin.py
+++ News/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Post, Category, Tag, Contact, NewsLetter
+from .models import Post, Category, Tag, Contact, NewsLetter, Comment
 
 admin.site.register(Post)
 admin.site.register(Category)
 admin.site.register(Tag)
 admin.site.register(Contact)
 admin.site.register(NewsLetter)
+admin.site.register(Comment)
